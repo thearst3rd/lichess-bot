5ad6c93d9f1eeced1152b00360dccc8448a5f917
Terry Hearst <thearst3rd@gmail.com>
2021-01-10 23:48:14 -0500

    Read stockfish path from env variable rather than silly .gitignored file

--- MultiBot/strategies/worstfish.py
+++ MultiBot/strategies/worstfish.py
@@ -1,6 +1,8 @@
 # A strategy that uses stockfish to play the worst moves possible
 # Created by Terry Hearst on 2020/12/30
 
+import os
+
 import chess
 import chess.engine
 
@@ -8,8 +10,12 @@ import strategy
 
 class WorstfishStrategy(strategy.BaseStrategy):
 	def __init__(self):
-		with open("../stockfishPath.txt") as f:
-			stockfishPath = f.read().strip()
+		try:
+			stockfishPath = os.environ["STOCKFISH_PATH"]
+		except KeyError:
+			print("ERROR")
+			print("Please set environment variable STOCKFISH_PATH to your stockfish executable")
+			os._exit(1)
 		self.engine = chess.engine.SimpleEngine.popen_uci(stockfishPath)
 
 	def get_move(self, board: chess.Board) -> chess.Move:
